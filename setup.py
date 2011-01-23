be6b02d20f604286b2c254d45c352ac7ac35b887
dstufft <donald.stufft@gmail.com>
2011-01-23 17:00:28 -0500

    added packaging information

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(name='hyde-zipper',
+version='0.0.1',
+description='gzip precompression as post processor',
+long_description=open('README.rst').read(),
+author='Donald von Stufft',
+author_email='donald.stufft@gmail.com',
+url='http://github.com/dstufft/hyde-zipper',
+classifiers=[
+  "Programming Language :: Python",
+  "Development Status :: 4 - Beta",
+  "Intended Audience :: Developers",
+],
+keywords='hyde html compressor',
+zip_safe=False,
+license='BSD',
+install_requires=[
+'setuptools',
+],
+packages = find_packages(),
+include_package_data = True,
+)
